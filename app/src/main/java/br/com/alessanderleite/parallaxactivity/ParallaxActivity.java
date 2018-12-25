6da4b170bdb8c70ccd864a7cb260535c213b2485
alessanderleite <alessander.logistica@gmail.com>
2018-12-25 12:21:26 -0200

    change the name of MainActivity to ParallaxActivity

--- app/src/main/java/br/com/alessanderleite/parallaxactivity/ParallaxActivity.java
+++ app/src/main/java/br/com/alessanderleite/parallaxactivity/ParallaxActivity.java
@@ -3,7 +3,7 @@ package br.com.alessanderleite.parallaxactivity;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class ParallaxActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
