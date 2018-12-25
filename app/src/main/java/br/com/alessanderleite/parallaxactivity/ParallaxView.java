08de4a0ea886bdf881214aeb8b99ac86fb56d1ca
alessanderleite <alessander.logistica@gmail.com>
2018-12-25 17:18:53 -0200

    implement the run and draw methods

--- app/src/main/java/br/com/alessanderleite/parallaxactivity/ParallaxView.java
+++ app/src/main/java/br/com/alessanderleite/parallaxactivity/ParallaxView.java
@@ -2,12 +2,17 @@ package br.com.alessanderleite.parallaxactivity;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.os.SystemClock;
 import android.util.AttributeSet;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 public class ParallaxView extends SurfaceView implements Runnable {
+
+    private volatile boolean running;
+    private Thread gameThread = null;
 
     // For drawing
     private Paint paint;
@@ -16,6 +21,9 @@ public class ParallaxView extends SurfaceView implements Runnable {
 
     // Holds a reference to the Activity
     Context context;
+
+    // Control the fps
+    long fps = 60;
 
     // Screen resolution
     int screenWidth;
@@ -37,6 +45,47 @@ public class ParallaxView extends SurfaceView implements Runnable {
 
     @Override
     public void run() {
+        while (running) {
+            long startFrameTime = System.currentTimeMillis();
 
+            update();
+
+            draw();
+
+            // Calculate the fps this frame
+            long timeThisFrame = System.currentTimeMillis() - startFrameTime;
+            if (timeThisFrame >= 1) {
+                fps = 1000 / timeThisFrame;
+            }
+        }
+    }
+
+    private void update() {
+        // Update all the background positions
+    }
+
+    private void draw() {
+
+        if (ourHolder.getSurface().isValid()) {
+            // First we lock the area of memory we will be drawing to
+            canvas = ourHolder.lockCanvas();
+
+            // draw a background color
+            canvas.drawColor(Color.argb(255, 0, 3, 70));
+
+            // draw the background parallax
+
+            // draw the rest of the game
+            paint.setTextSize(60);
+            paint.setColor(Color.argb(255, 255, 255, 255));
+            canvas.drawText("I am a plane", 350, screenHeight / 100 * 5, paint);
+            paint.setTextSize(220);
+            canvas.drawText("I am a train", 50, screenHeight / 100 * 80, paint);
+
+            // draw the foreground parallax
+
+            // unlock and draw the scene
+            ourHolder.unlockCanvasAndPost(canvas);
+        }
     }
 }
